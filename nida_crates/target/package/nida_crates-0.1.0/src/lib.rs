700b10e84a8cba82113d741d70c73b006b6e8119
Nida Khan <knida5637@gmail.com>
2019-10-25 14:48:18 +0000

    my first nida shower commit

--- nida_crates/target/package/nida_crates-0.1.0/src/lib.rs
+++ nida_crates/target/package/nida_crates-0.1.0/src/lib.rs
@@ -1,5 +1,5 @@
 pub fn print_fun(uper:i32,lower:i32){
     for index in uper..lower{
-        println!("Downward:{}",index);
+        println!("Downward:{}"index);
     }
 }
---
+++ nida_crates/target/package/nida_crates-0.1.0/target/debug/deps/nida_crates-05a72597cb641d1f.d
@@ -0,0 +1,7 @@
+/home/nida/Desktop/shower_nida/nida_shower/nida_crates/target/package/nida_crates-0.1.0/target/debug/deps/nida_crates-05a72597cb641d1f.rmeta: src/lib.rs
+
+/home/nida/Desktop/shower_nida/nida_shower/nida_crates/target/package/nida_crates-0.1.0/target/debug/deps/libnida_crates-05a72597cb641d1f.rlib: src/lib.rs
+
+/home/nida/Desktop/shower_nida/nida_shower/nida_crates/target/package/nida_crates-0.1.0/target/debug/deps/nida_crates-05a72597cb641d1f.d: src/lib.rs
+
+src/lib.rs:
